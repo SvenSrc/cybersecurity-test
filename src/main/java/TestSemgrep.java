7aaca16c1ba595ddba97d13ff9a21c97729c1ad0
Sven <69164233+SvenSrc@users.noreply.github.com>
2025-10-16 15:18:10 +0200

    Create TestSemgrep.java

---
+++ src/main/java/TestSemgrep.java
@@ -0,0 +1,6 @@
+public class TestSemgrep {
+    public static void main(String[] args) {
+        String password = "123456"; // Hardcoded secret
+        System.out.println("Password is: " + password);
+    }
+}
